3d8806f2032abe174a5f85d844a31465504f10c8
alexharrisonmusic <alexanderjosephharrison@gmail.com>
2020-01-14 22:39:49 -0800

    added code to make sure this works BOTH with ajax call to website as well as local html copy

--- updateprices.js
+++ updateprices.js
@@ -9,41 +9,31 @@ var request = require("request");
 
   // Set the configuration for your app
   // TODO: Replace with your project's config object
-  var config = {
-    apiKey: "apiKey",
-    authDomain: "projectId.firebaseapp.com",
-    databaseURL: "https://databaseName.firebaseio.com",
-    storageBucket: "bucket.appspot.com"
-  };
-  firebase.initializeApp(config);
+  // var config = {
+  //   apiKey: "apiKey",
+  //   authDomain: "projectId.firebaseapp.com",
+  //   databaseURL: "https://databaseName.firebaseio.com",
+  //   storageBucket: "bucket.appspot.com"
+  // };
 
-  // Get a reference to the database service
-  var database = firebase.database();
+  // var firebaseConfig = {
+  //   apiKey: "AIzaSyAAl4HoMLITuHncLnN9OMSSKhjOXnx0C7Q",
+  //   authDomain: "train-time-a2fb5.firebaseapp.com",
+  //   databaseURL: "https://train-time-a2fb5.firebaseio.com",
+  //   projectId: "train-time-a2fb5",
+  //   storageBucket: "train-time-a2fb5.appspot.com",
+  //   messagingSenderId: "907721756168"
+  // };
+  // firebase.initializeApp(config);
+
+  // // Get a reference to the database service
+  // var database = firebase.database();
 
 
 //when it's live actually query the site but for now just use the saved local data for dev
 var options = {
   url: "https://www.medicaltourism.com/compare-prices"
 };
-
-// function callback(error, response, body) {
-//   if (!error && response.statusCode == 200) {
-//     console.log(body);
-//     //parse through and handle each tab and assign the children a variable
-
-//     //<--------------------------------FREE ME
-
-//     //<---------------------------------------PUT THE root=HTMLParser.parse(body) Logic here
-//     //<-------------------------------parse the dom into data objects here!!!
-//     var root = HTMLParser.parse(body);
-
-//     var tabTitles = root.querySelector(".procedure-tab");
-//     console.log("tabTitles\n");
-//     console.log(tabTitles);
-//     //save all that data in a JSON object built from it
-//   }
-// }
-// request(options, callback);
 var path = "curlmedicaltourismcompareprices.html";
 var fs = require("fs");
 
@@ -52,71 +42,93 @@ var fs = require("fs");
 // SAMPLE DATA OBJECT
 // procedures['IVF Treatment']["Columbia"]: "USD $7100",
 
+var body;
 
-var body = fs.readFileSync(path, "utf8");
+//uncomment the below two lines to run on local html copy instead of performing ajax call
+// body = fs.readFileSync(path, "utf8");
+// process(body);
+
+
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log("resbody");
+    process(body);
+
+    //save all that data in a JSON object built from it
+  }
+}
+request(options, callback);
+console.log("should be updated w body")
+// console.log(body);
 // console.log(body);
 // var body = '';
 
-var root = HTMLParser.parse(body);
+function process(body){
+  var root = HTMLParser.parse(body);
 
-var tabTitles = root.querySelector(".tabs-menu");
-// console.log("tabTitles\n");
-var tables = tabTitles.childNodes;
-var bigDATAobj = {};
-var holder = {};
-bigDATAobj.Procedures = {};
-for (let i = 0; i < tables.length; i++) {
-  var tableTitles = tables[i].text;
-  if (!tableTitles.includes("\n")){
-    bigDATAobj.Procedures[tableTitles]={};
-  // console.log(tableTitles);
+  var tabTitles = root.querySelector(".tabs-menu");
+  // console.log("tabTitles\n");
+  var tables = tabTitles.childNodes;
+  var bigDATAobj = {};
+  var holder = {};
+  bigDATAobj.Procedures = {};
+  for (let i = 0; i < tables.length; i++) {
+    var tableTitles = tables[i].text;
+    if (!tableTitles.includes("\n")){
+      bigDATAobj.Procedures[tableTitles]={};
+    // console.log(tableTitles);
+    }
+    
+  }
+  var countryCostArr = root.querySelectorAll(".tab-pane-container");//only grabs the first
+  
+  for (let j = 0; j < countryCostArr.length; j++) {
+      var priceObject = {};
+    var children = countryCostArr[j].childNodes;
+      for (let k = 0; k < children.length; k++) {
+        if (children[k] != undefined){
+          var thisone = children[k].childNodes;        
+          for (let m = 0; m < thisone.length; m++) {
+            var currPriceMatch = {};
+            var nameNow;
+            var priceNow;
+            if (thisone[m] != undefined){
+              var thisoneNOW = thisone[m];
+             if (thisoneNOW.nodeType == 1){
+                if ((!thisoneNOW.text.includes("$")) &&(!thisoneNOW.text.includes("\n"))){
+                  nameNow = thisoneNOW.text;
+                  priceObject[nameNow] = 0;
+                }else if (thisoneNOW.text.includes("$")){
+                  priceNow = thisoneNOW.text;
+                  priceObject[nameNow] = priceNow;
+                };
+              }
+            }
+        }
+        }
+    }
+  
+    holder[j] = priceObject; // creating bigDataobj.['0'].Colombia for example wtf
+    // console.log("priceObject")
+    // console.log(priceObject);
   }
   
+  
+  Object.keys(bigDATAobj.Procedures).forEach(function(key, idx){
+    bigDATAobj.Procedures[key] = holder[idx];
+  });
+  console.log("bigDATAobj")
+  console.log(bigDATAobj);
+  //access Firebase
+
+  //insert into firebase data object
+  
 }
-var countryCostArr = root.querySelectorAll(".tab-pane-container");//only grabs the first
-
-for (let j = 0; j < countryCostArr.length; j++) {
-    var priceObject = {};
-  var children = countryCostArr[j].childNodes;
-    for (let k = 0; k < children.length; k++) {
-      if (children[k] != undefined){
-        var thisone = children[k].childNodes;        
-        for (let m = 0; m < thisone.length; m++) {
-          var currPriceMatch = {};
-          var nameNow;
-          var priceNow;
-          if (thisone[m] != undefined){
-            var thisoneNOW = thisone[m];
-           if (thisoneNOW.nodeType == 1){
-              if ((!thisoneNOW.text.includes("$")) &&(!thisoneNOW.text.includes("\n"))){
-                nameNow = thisoneNOW.text;
-                priceObject[nameNow] = 0;
-              }else if (thisoneNOW.text.includes("$")){
-                priceNow = thisoneNOW.text;
-                priceObject[nameNow] = priceNow;
-              };
-            }
-          }
-      }
-      }
-  }
-
-  holder[j] = priceObject; // creating bigDataobj.['0'].Colombia for example wtf
-  // console.log("priceObject")
-  // console.log(priceObject);
-}
-
-
-Object.keys(bigDATAobj.Procedures).forEach(function(key, idx){
-  bigDATAobj.Procedures[key] = holder[idx];
-});
-console.log("bigDATAobj")
-console.log(bigDATAobj);
 
 
 
 // TODO:
-// 1 - assemble data object from HTML page data
+// x1 - assemble data object from HTML page data
 // 2 - Upload to firebase
 // 3 - add sort function to the object before upload to firebase to make it easier to search results
 // 4 - write description of how its sorted to ease data sorting
