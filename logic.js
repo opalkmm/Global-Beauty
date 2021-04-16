deb1dcf69c71557478dba790c59c0788f2c4f253
Ornrumpa Kanchanomai <opalkm@Ornrumpas-MacBook-Pro.local>
2021-04-16 11:24:57 -0700

    revise logic file

--- logic.js
+++ logic.js
@@ -1,29 +1,31 @@
- 
- //style slider
- var slider = document.getElementById("myRange");
- var output = document.getElementById("value");
+//style slider
+var slider = document.getElementById("myRange");
+var output = document.getElementById("value");
 
- output.innerHTML = slider.value;
- 
+output.innerHTML = slider.value;
 
- slider.oninput = function(){
-   output.innerHTML = this.value;
- }
- 
- slider.addEventListener("mousemove", function(){
-   var x = slider.value;
-   var percent = x/10;
-   var color = 'linear-gradient(90deg, rgb(0,0,0)' + percent + '%,' + 'rgb(214,214,214)' + percent + '%)';
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
+
+slider.addEventListener("mousemove", function () {
+  var x = slider.value;
+  var percent = x / 10;
+  var color =
+    "linear-gradient(90deg, rgb(0,0,0)" +
+    percent +
+    "%," +
+    "rgb(214,214,214)" +
+    percent +
+    "%)";
   slider.style.background = color;
 
-  console.log(percent)
-  console.log(color)
+  console.log(percent);
+  console.log(color);
+});
 
-  })
- 
- 
- //initialize firebase
- const firebaseConfig = {
+//initialize firebase
+const firebaseConfig = {
   apiKey: "AIzaSyC1tZPmoOueA0xSyULNNt4fgxoh7FFTzLs",
   authDomain: "root-pixel-243301.firebaseapp.com",
   databaseURL: "https://root-pixel-243301.firebaseio.com",
@@ -39,35 +41,23 @@ firebase.initializeApp(firebaseConfig);
 //Get a reference to the database service
 var database = firebase.database();
 
-
-$(document).ready(function() {
- 
-
+$(document).ready(function () {
   //add event listener to the click for the ajax call
-  $("#submit").on("click", function(e) {
+  $("#submit").on("click", function (e) {
     e.preventDefault();
     console.log("button works");
 
-    
-//get users' chosen procedure
+    //get users' chosen procedure
 
-let procedure = document.getElementByName('optionsRadios');
+    let procedure = document.getElementByName("optionsRadios");
 
-for(i = 0; i < procedure.length; i++){
-  if(procedure[i].checked){
-    // document.getElementById('cost').innerHTML= "chosen: " + procedure[i].value
-    console.log(procedure.value)
-  }
+    for (i = 0; i < procedure.length; i++) {
+      if (procedure[i].checked) {
+        // document.getElementById('cost').innerHTML= "chosen: " + procedure[i].value
+        console.log(procedure.value);
+      }
+    }
 
-}
-
-
-
-
-
-
-
-    
     //get destination input radio button (capital city airport code)
 
     var flightCountry = $("input[name='country']:checked").val();
@@ -86,13 +76,10 @@ for(i = 0; i < procedure.length; i++){
 
     var today = new Date();
     var startDate = moment().format("YYYY-MM-DD");
-    var endDate = moment(startDate)
-      .add(1, "months")
-      .format("YYYY-MM-DD");
+    var endDate = moment(startDate).add(1, "months").format("YYYY-MM-DD");
 
     console.log(startDate);
     console.log(endDate);
-
 
     var flights = {
       async: true,
@@ -118,59 +105,61 @@ for(i = 0; i < procedure.length; i++){
     };
     console.log(flights);
 
-    $.ajax(flights).done(function(response) {
+    $.ajax(flights).done(function (response) {
       //get the minimum price of the flight from the selected country
       console.log(response.Quotes[0].MinPrice);
 
       //*******DON'T FORGET TO LOOP THROUGH INDEX******************************************
 
       let minPrice = response.Quotes[0].MinPrice;
-    //surgery type and cost
+      //surgery type and cost
       let surCost = 1;
       let hotelCost = 2;
       //*******DON'T FORGET TO LOOP THROUGH WHEN MULTIPLE COUNTRIES ARE SELECTED***********
 
       //create function to append prices dynamically
 
-      function addRow(tableID){
-      var total = surCost + minPrice + hotelCost
-      //append cost table
-        $('#table > tbody:last-child').append('<tr>' + '<td>' + surCost + '</td>' + '<td>' + minPrice + '</td>'+ '<td>' + hotelCost + '</td>' + '<td>' + total + '</td>' +  '</tr>')
-
-        
-
+      function addRow(tableID) {
+        var total = surCost + minPrice + hotelCost;
+        //append cost table
+        $("#table > tbody:last-child").append(
+          "<tr>" +
+            "<td>" +
+            surCost +
+            "</td>" +
+            "<td>" +
+            minPrice +
+            "</td>" +
+            "<td>" +
+            hotelCost +
+            "</td>" +
+            "<td>" +
+            total +
+            "</td>" +
+            "</tr>"
+        );
       }
       // Call addRow() with the table's ID
-        addRow('table');
+      addRow("table");
       console.log("MINPRICE : " + minPrice);
     });
 
-//pulling data for the hotel in the selected countries
+    //pulling data for the hotel in the selected countries
 
-// let hotels = {
-//   async: true,
-//   crossDomain: true,
-//   url: "https://www.googleapis.com/travelpartner/v2.0/4200042/prices/1066",
-//   method: "GET"
-// }
+    // let hotels = {
+    //   async: true,
+    //   crossDomain: true,
+    //   url: "https://www.googleapis.com/travelpartner/v2.0/4200042/prices/1066",
+    //   method: "GET"
+    // }
 
-// //key:249162058472-4d7hcb6dqt4tp0emmok4rqrnu1isfdo2.apps.googleusercontent.com
+    // console.log(hotels);
 
-// console.log(hotels);
+    // $.ajax(hotels).done(function(response) {
 
-// $.ajax(hotels).done(function(response) {
-  
-//   var hotelPrices = hotels.response.Prices
-//   console.log(hotels.hotelPrices);
+    //   var hotelPrices = hotels.response.Prices
+    //   console.log(hotels.hotelPrices);
 
-// });
-
-
-
-
-
-
-
-
+    // });
   });
 });
